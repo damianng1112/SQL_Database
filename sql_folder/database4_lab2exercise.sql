7b7247cd36ae27907d706d379e55112ca4a32566
Wye Zhen Ng <damianng1112@gmail.com>
2024-09-18 09:34:43 +0100

    solutions for lab2exercise

--- sql_folder/database4_lab2exercise.sql
+++ sql_folder/database4_lab2exercise.sql
@@ -39,44 +39,66 @@ INSERT INTO details VALUES ( null, 'Justin', 'Maguire', 25, 'M', 'Lecturer', 'Bu
 INSERT INTO details VALUES ( null, 'Jacqulinen', 'Musgrave', 43, 'F', 'Professor', 'Business', 76.45, 10);
 INSERT INTO details VALUES ( null, 'Julia', 'Moore', 36, 'F', 'Administrator', 'Science', 45.57, 38);
 
-select * from details where age >= 40 and age <=50;
+#1.	Get the details of all employees who are not between the ages of 40 and 50.
+select * from details where age < 40 or age > 50;
+select * from details where age not between 40 and 50;
 
+#2.	Get the details of all employees who work between the 10 and 15 hours per week.
 select * from details where hours >= 10 and hours <=15;
 
+#3.	Find all employees who have an ‘e’ in their first name.
 select * from details where firstName like '%e%';
 
-select * from details where firstName like '_e%';
+#4.	Find all employees whose second letter of their first name is ‘u’
+select * from details where firstName like '_u%';
 
+/*5.	Find all employees whose first name :
+	ends with n and is only 4 letters in length
+    starts with a ‘J’ but is only 4 letters in length
+	is only 3 letters in length
+    is greater than 4 letters in length
+*/
 select * from details where firstName like '%n' and LENGTH(firstName) = 4;
-
-select * from details where firstName like 'J%' and LENGTH(firstName) = 4;
-
+select * from details where firstName like 'j%' and LENGTH(firstName) = 4;
 select * from details where LENGTH(firstName) = 3;
-
 select * from details where LENGTH(firstName) >= 4;
 
+#6.	Find all female employees who have a ‘3’ in their age.
 select * from details where gender = 'F' having age like '%3%';
 
+#7.	Count the number of females.
 select COUNT(gender) as 'Number Of Females' from details where gender = 'F';
 
+#8.	Count the number of males.
 select COUNT(gender) as 'Number Of Males' from details where gender = 'M';
 
+#9.	Calculate the average age of the females.
 select avg(age) as 'Average Age of Females' from details where gender = 'F';
 
+#10.	Calculate the average age of the males.
 select avg(age) as 'Average Age of Females' from details where gender = 'M';
 
-select * from details where age = (select MAX(age) from details);
+#11.	Who is the oldest employee?
+select * from details where age in (select MAX(age) from details);
+select * from details order by age desc limit 1;
 
-select * from details where age = (select min(age) from details);
+#12.	Who is the youngest employee?
+select * from details where age in (select min(age) from details);
 
+#13.	What is the average hours worked?
 select avg(hours) as 'Average hours worked' from details;
 
+#14.	What is the total of the female wage?
 select sum(rate) as 'total wage of females' from details where gender = 'F';
 
+#15.	What is the total of the male wage?
 select sum(rate) as 'total wage of males' from details where gender = 'M';
 
+#16.	What is the average age in each department?
 select avg(age) as 'average age', department from details group by department;
 
+#17.	What is the average age of each position?
 select avg(age) as 'average age', position from details group by position;
 
+#18.	Count the number of males/females per department.
 select department, gender, count(gender) as totalCount from details group by department, gender order by department;
